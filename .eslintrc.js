fd910979607a719436cbb1f818fb22bc24df1c54
caesar <caesar.luo@silksoftware.com>
2021-10-16 22:18:36 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,36 +14,36 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    semi: ['error', 'never'],
-    camelcase: 0,
-    'lines-between-class-members': [
-      'error',
-      'always',
-      { exceptAfterSingleLine: true }
-    ],
-    'no-unused-vars': 'off',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-param-reassign': 0,
-    'no-underscore-dangle': 0,
-    'no-plusplus': 0,
-    'no-useless-escape': 0,
-    'consistent-return': 0,
-    'global-require': 0,
-    'space-before-function-paren': 'off',
-    'object-shorthand': ['error', 'always', { avoidQuotes: true }],
-    'prettier/prettier': [
-      'error',
-      {
-        semi: false,
-        singleQuote: true,
-        usePrettierrc: false,
-        trailingCommas: 'none',
-        fileInfoOptions: {
-          withNodeModules: true
-        }
-      }
-    ],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    // semi: ['error', 'never'],
+    // camelcase: 0,
+    // 'lines-between-class-members': [
+    //   'error',
+    //   'always',
+    //   { exceptAfterSingleLine: true }
+    // ],
+    // 'no-unused-vars': 'off',
+    // 'no-console': ['error', { allow: ['warn', 'error'] }],
+    // 'no-param-reassign': 0,
+    // 'no-underscore-dangle': 0,
+    // 'no-plusplus': 0,
+    // 'no-useless-escape': 0,
+    // 'consistent-return': 0,
+    // 'global-require': 0,
+    // 'space-before-function-paren': 'off',
+    // 'object-shorthand': ['error', 'always', { avoidQuotes: true }],
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     semi: false,
+    //     singleQuote: true,
+    //     usePrettierrc: false,
+    //     trailingCommas: 'none',
+    //     fileInfoOptions: {
+    //       withNodeModules: true
+    //     }
+    //   }
+    // ],
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/no-unused-vars': 'off'
   }
 }
