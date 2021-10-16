5a91a58ec076d2d44676ed41aa9dee6423488e5d
caesar <caesar.luo@silksoftware.com>
2021-10-16 22:17:59 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,14 +14,21 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    camelcase: 'off',
+    semi: ['error', 'never'],
+    camelcase: 0,
     'lines-between-class-members': [
       'error',
       'always',
       { exceptAfterSingleLine: true }
     ],
-    'no-console': 'off',
     'no-unused-vars': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+    'no-plusplus': 0,
+    'no-useless-escape': 0,
+    'consistent-return': 0,
+    'global-require': 0,
     'space-before-function-paren': 'off',
     'object-shorthand': ['error', 'always', { avoidQuotes: true }],
     'prettier/prettier': [
