7b5ec2e683190cb7c053b9c3930d4685619aa2ac
caesar <caesar.luo@silksoftware.com>
2021-10-16 20:20:09 +0800

    chore: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    // camelcase: 'off',
+    // 'lines-between-class-members': [
+    //   'error',
+    //   'always',
+    //   { exceptAfterSingleLine: true }
+    // ],
+    // 'no-console': 'off',
+    // 'no-unused-vars': 'off',
+    // 'space-before-function-paren': 'off',
+    // 'object-shorthand': ['error', 'always', { avoidQuotes: true }],
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     semi: false,
+    //     singleQuote: true,
+    //     usePrettierrc: false,
+    //     trailingCommas: none,
+    //     fileInfoOptions: {
+    //       withNodeModules: true
+    //     }
+    //   }
+    // ],
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/no-unused-vars': 'off'
+  }
+}
